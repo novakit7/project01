5c644af5225644b74203943d985fe5d7e54b574d
Mayank Kumar Tiwari <mayankkumartiwari05042005@gmail.com>
2026-03-21 01:43:21 +0530

    home if changed

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
-
+import '../App.css'
 export default function Home() {
   return (
-    <div>
-      thsi is home
+    <div className="h-main">
+      {/* top of home main */}
+     <div className="h-top">
+      <div className="h-backlog">
+        <p> BACKLOGS </p>
+        <span className="h-backcounts">
+          <h3>05</h3>
+          <p>jan 13- march8</p>
+        </span>
+        <span className="h-backlogimage"></span>
+          
+      </div>
+      <div className="h-pending"></div>
+      <div className="h-complete"></div>
+      <div className="h-ongoing"></div>
+     </div>
+      {/* middle of home */}
+      <div className="h-main">
+
+      </div>
+      {/* end of home */}
     </div>
   )
 }
