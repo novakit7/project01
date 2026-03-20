e6aca49635e38bfb4a3c22e40dd2ccb34dc0e9a8
lokesh kumar <lokeshkumar120919@gmail.com>
2026-03-21 00:15:07 +0530

    fixed footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,54 +1,30 @@
-import React from 'react'
-import '../App.css'
 
+import "../App.css";
 
-export default function() {
+export default function () {
   return (
-    <>
-        
-        {/* <div className='f-container' >
-        <div className='f-box' > 
-           <div> © 2026 To Do List App</div>
-           <div className='f-mid'>
-            <a>To Do </a>
-            <a>link1 </a>
-            <a>link2</a>
-           </div>
-           <div><a>Gmail</a></div>
+      <footer className="footer">
+        <div className="footer-container">
+          <div className="footer-left">
+            <h2>To-Do</h2>
+            <p>Stay organized, stay productive.</p>
+          </div>
+
+          <div className="footer-center">
+            <a href="#">About</a>
+            <a href="#">Help</a>
+            <a href="#">Privacy</a>
+            <a href="#">Terms</a>
+          </div>
+
+          <div className="footer-right">
+            <p>Version 1.0</p>
+          </div>
         </div>
-        <div id='back'><a>Back to top</a></div>
-        </div> */}
-        <footer class="footer">
-  <div class="footer-container">
-    
-    <div class="footer-left">
-      <h2>TaskFlow</h2>
-      <p>Stay organized, stay productive.</p>
-    </div>
 
-    <div class="footer-center">
-      <a href="#">About</a>
-      <a href="#">Help</a>
-      <a href="#">Privacy</a>
-      <a href="#">Terms</a>
-    </div>
-
-    <div class="footer-right">
-      <p>Version 1.0</p>
-      <div class="social-icons">
-        <a href="#">🌐</a>
-        <a href="#">🐦</a>
-        <a href="#">📘</a>
-      </div>
-    </div>
-
-  </div>
-
-  <div class="footer-bottom">
-    <p>© 2026 TaskFlow. All rights reserved.</p>
-  </div>
-</footer>
-
-    </>
-  )
-};
+        <div className="footer-bottom">
+          <p>&copy; 2026 TaskFlow. All rights reserved.</p>
+        </div>
+      </footer>
+  );
+}
