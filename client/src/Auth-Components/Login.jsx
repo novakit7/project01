681b692cea0b2afea20c6ace6c4ebc6dae93343d
Mayank Kumar Tiwari <mayankkumartiwari05042005@gmail.com>
2026-03-21 23:27:07 +0530

    login signup added

--- client/src/Auth-Components/Login.jsx
+++ client/src/Auth-Components/Login.jsx
@@ -3,8 +3,23 @@ import "./Auth.css"
 
 export default function Login() {
   return (
-    <div>
-      this is login
+      <div className="s-container">
+     <div className="signup-box loginbox">
+      <h2>🔐 Login</h2>
+
+      <form>
+        <input type="email" placeholder="Enter Email" required />
+
+        <input type="password" placeholder="Enter Password" required />
+
+        
+        <button type="submit">Login</button>
+
+        <p className="switch-text">
+          Don't have an account? <a href="#">Sign Up</a>
+        </p>
+      </form>
+    </div>
     </div>
   )
 }
